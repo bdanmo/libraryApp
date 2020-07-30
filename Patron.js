aedcf99f70cd8f31f6837592c49a1c328b40d7f4
Brendan Moran <b.daniel.moran@gmail.com>
2020-07-29 20:01:45 -0400

    Added jsdoc to Patron class

--- Patron.js
+++ Patron.js
@@ -6,12 +6,20 @@ class Patron {
     this.balance = 0;
   }
 
+  /**
+   * Assigns a book to patron's current book property
+   * @param {Book} book - an instance of the Book class to be added
+   */
   checkOut(book) {
     this.currentBook = book;
     book.checkedOut = true;
     book.patron = this;
   }
 
+  /**
+   * Removes a book from patron's current book property
+   * @param {Book} book - an instance of the Book class to be removed
+   */
   returnBook(book) {
     this.currentBook = null;
     book.checkedOut = false;
